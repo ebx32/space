20ad650124add2077a60961518919193faf97399
Callum <292811883+ebx32@users.noreply.github.com>
2026-07-13 12:48:04 +0530

    fix links v2

--- astro.config.ts
+++ astro.config.ts
@@ -12,7 +12,7 @@ import { headingNamespace } from "./src/lib/heading-namespace"
 import { headingAnchors } from "./src/lib/heading-anchors"
 
 export default defineConfig({
-    site: "https://ebx32.github.io/blog",
+    site: "https://ebx32.github.io",
     base: "/blog",
   compressHTML: true,
   prefetch: { prefetchAll: true },
